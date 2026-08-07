06883c9c98b29623bfeeabf787031a051f43f790
Professor Minty <speyton561@gmail.com>
2026-08-07 03:12:04 -0400

    feat: publish maximum-shelf environmental runtime .6

---
+++ dist/v0.1.0-black-hole-lab.6/black-hole-museum.js
@@ -0,0 +1,1 @@
+export { mountBlackHoleMuseum } from 'https://cdn.jsdelivr.net/gh/ProfessorMinty/HughesWebAssets@0b50187fe1a3aa6f0d63018d332150abc0d43bb9/apps/black-hole-museum/src/maximum-shelf-runtime.js';
