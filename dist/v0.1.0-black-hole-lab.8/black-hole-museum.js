ac9768ef781054d5bbd80c96565e98fa4a06711b
Professor Minty <speyton561@gmail.com>
2026-08-07 04:02:14 -0400

    release: add current-plus-one runtime for .8

---
+++ dist/v0.1.0-black-hole-lab.8/black-hole-museum.js
@@ -0,0 +1,1 @@
+export { mountBlackHoleMuseum } from 'https://cdn.jsdelivr.net/gh/ProfessorMinty/HughesWebAssets@fbd091eab0eca5f2922b7e45eb43836ab37ec96a/apps/black-hole-museum/src/maximum-shelf-runtime.js';
