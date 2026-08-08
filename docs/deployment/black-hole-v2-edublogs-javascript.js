017a53e68da43fe1a122215ee2a0cc9751a9e06e
Professor Minty <speyton561@gmail.com>
2026-08-07 23:09:21 -0400

    deploy: pin Edublogs loader to black hole V2 .5

--- docs/deployment/black-hole-v2-edublogs-javascript.js
+++ docs/deployment/black-hole-v2-edublogs-javascript.js
@@ -7,8 +7,8 @@
   const ROOT_ID = 'hrv-black-hole-v2-root';
   const PAGE_ID = 'repository-page-lab-black-holes-v2';
   const PAGE_SYSTEM = 'black-hole-museum-v2';
-  const EXPECTED_RELEASE = '0.2.0-black-hole-v2-lab.4';
-  const RELEASE_MANIFEST = 'https://cdn.jsdelivr.net/gh/ProfessorMinty/HughesWebAssets@21e8ad037509a4cb530000a8564ff4a6ce431e28/dist/v0.2.0-black-hole-v2-lab.4/release.json';
+  const EXPECTED_RELEASE = '0.2.0-black-hole-v2-lab.5';
+  const RELEASE_MANIFEST = 'https://cdn.jsdelivr.net/gh/ProfessorMinty/HughesWebAssets@4c8cd3833a79d2777b91ab7d1c5363dbefc6895a/dist/v0.2.0-black-hole-v2-lab.5/release.json';
   const TIMEOUT_MS = 12000;
 
   function ready(callback) {
